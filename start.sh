9db2ffff493343bb9f8bee57b8f26769c139c7fc
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2022-03-07 22:52:01 +0100

    Delete start.sh
    clearing project

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "++++++++Git installation and clonning PHT Weather Station Backend repository+++++++++"
-apt install git
-git clone https://github.com/wojciechignasiak/P_H_T_Weather_Station_Backend.git
-chmod -R 777 ./P_H_T_Weather_Station_Backend
-echo "Now please type cd P_H_T_Weather_Station_Backend and press Enter"
-sleep 3
-
