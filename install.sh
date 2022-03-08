55d6ae3dac7cc0ffec898c2bea5b774c4f152bf6
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2022-03-08 22:56:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,13 @@
 apt install git
 apt install python3-pip
 echo "++++Python3 with pip installation complete++++"
-apt install curl
-echo "++++Curl installation complete++++"
 echo "++++Docker set up++++"
 apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -20,7 +18,6 @@ groupadd docker
 usermod -aG docker $USER
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-chmod 777 *
 echo "++++Docker and Docker-Compose installation complete.++++"
 echo "++++System has been prepared for PHT deployment++++"
 echo "++++Please reboot your device to update users group”
